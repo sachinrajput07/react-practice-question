6c992cb05629e35a57e06feaab2371b83a29abc3
sachinrajput07 <sachinsingh2834@gmail.com>
2024-03-13 18:56:22 +0530

    its a practice question

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './index.css'
+
 
 function App() {
+
+  let [Counter, setcounter] = useState(15);
+
+  let addValue = ()=>{
+    if (Counter <= 20) {
+      setcounter(Counter + 1)
+    }
+    
+  }
+  let removeValue = ()=> {
+    if (Counter >= 1) {
+      setcounter(Counter - 1)
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Counter Update : {Counter}</h1>
+    <button onClick={addValue}>Increament</button>
+    <button onClick={removeValue}>Dcreament</button>
     </div>
-  );
+  )   
 }
 
 export default App;
